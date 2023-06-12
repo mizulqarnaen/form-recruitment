5f4cfef1cae380f6b4e6d4c5e917d25a67933d53
Mohammad Iqbal <mohammad.iqbal@neuronworks.co.id>
2023-06-12 23:03:20 +0700

    menambahkan disable button submit ketika quota kosong

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -86,16 +86,20 @@ function checkQuota() {
     const selectedOption = selectElement.options[selectElement.selectedIndex];
     const vacancyName = '<strong>' + selectedOption.text + '</strong>';
     const vacancyQuota = selectedOption.getAttribute("data-value");
+    const btnSubmit = document.getElementById('btnSubmit');
 
     if(vacancyQuota == 0) {
         let text = 'Mohon maaf, rekrutasi untuk '+ vacancyName + ' sudah penuh. dan tidak dapat dipilih.';
         showErrorText('positionErrorText', text, 'danger', 'success');
+        btnSubmit.disabled = true;
     } else if (vacancyQuota > 0 && vacancyQuota <= 2) {
         let text = 'Kuota tersisa untuk '+ vacancyName + ' hanya ' + vacancyQuota + ' pendaftar.';
         showErrorText('positionErrorText', text, 'danger', 'success');
+        btnSubmit.disabled = false;
     } else {
         let text = 'Anda dapat memilih lowongan '+ vacancyName + '.';
         showErrorText('positionErrorText', text, 'success', 'danger');
+        btnSubmit.disabled = false;
     }
 
 }
